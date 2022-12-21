c563bd689aba2148d59de8483214da5b3bd98d84
Audrey Houghton <hough129@umn.edu>
2022-12-21 17:53:07 -0600

    debugged some of the errors - will only run with copy paste, not as a script

--- error_query/dicom2bids_error_query.sh
+++ error_query/dicom2bids_error_query.sh
@@ -1,5 +1,4 @@
-
-#/bin/bash
+#!/bin/bash
 
 error_directory=$1
 output_path=$2
@@ -15,13 +14,13 @@ uniq_subjects=$( cat ${output_path}/${all_subjects_file} | sort | uniq )
 time_limit_subjects=( )
 oom_subjects=( )
 
-for subject in uniq_subjects; do
-$subject_files=$( grep -m 1 $subject *.err | cut -d: -f 1 ; ) ;
+for subject in ${uniq_subjects}; do
+subject_files=$( grep -m 1 $subject ${error_directory}/*.err | cut -d: -f 1 ) ;
 most_recent_file_info=$( ls -lrt $subject_files | head -n 1 ) ;
 most_recent=$( basename $( echo $most_recent_file_info | awk '{print $NF}' ))
-#echo $most_recent
-time_limit_subjects=$( grep -l "DUE TO TIME LIMIT" $most_recent | cut -d_ -f2 | cut -d. -f1); #make if statement to grab run number if "DUE TO TIME LIMIT" is outputted
-oom_subjects=$( grep -l "oom-kill" $most_recent | cut -d_ -f2 | cut -d. -f1 );
+echo $most_recent
+time_limit_subjects=$( grep -l "DUE TO TIME LIMIT" ${error_directory}/${most_recent} | cut -d_ -f2 | cut -d. -f1); #make if statement to grab run number if "DUE TO TIME LIMIT" is outputted
+oom_subjects=$( grep -l "oom-kill" ${error_directory}/${most_recent} | cut -d_ -f2 | cut -d. -f1 );
 done
 
 rm ${output_path}/${all_subjects_file}
