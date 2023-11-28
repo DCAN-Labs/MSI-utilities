658fe4b4a741cc29126cec53ace402606cadfd5e
rae McCollum <mccol199@acn17.agate.msi.umn.edu>
2023-11-28 16:33:02 -0600

    added more cubids query scripts

---
+++ query_cubids_errors/create_subject_lists.py
@@ -0,0 +1,35 @@
+"""
+Creates subject lists of cubids errors 
+"""
+import os
+
+def extract_subject_from_files(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith('-errors.txt'):
+            input_filepath = os.path.join(directory, filename)
+            error_name = filename.split('-')[0]
+
+            # Read the first column of each line in the file
+            with open(input_filepath, 'r') as infile:
+                lines = infile.readlines()
+
+            subjects_and_sessions = set()
+            for line in lines:
+                # Extract subject and session from the first column
+                parts = line.split('/')
+                subject = parts[1]
+                session = parts[2]
+                subjects_and_sessions.add(f'{subject},{session}')
+
+            # Write subjects to a new file
+            output_filepath = os.path.join(directory, f'{error_name}-subjects.txt')
+            with open(output_filepath, 'w') as outfile:
+                outfile.write('\n'.join(subjects_and_sessions))
+
+            print(f"Processed {filename}. Extracted subjects written to {output_filepath}")
+
+# Example usage:
+input_directory_path = '/home/rando149/shared/projects/rae_testing/abcc_s3_cubids/cubids_errors/year2_errors/'  # Replace with your input directory path
+output_directory_path = '/path/to/your/output/directory'  # Replace with your output directory path
+
+extract_subject_from_files(input_directory_path)
--- query_cubids_errors/filter-cubids-errors.py
+++ query_cubids_errors/filter-cubids-errors.py
@@ -8,9 +8,10 @@ or grabs lines that have a specific value.
 import csv
 
 def remove_lines(tsv_input, tsv_output):
+    """Filters out lines containing unwanted error strings"""
     with open(tsv_input, 'r', newline='') as input_file:
         reader = csv.reader(input_file, delimiter='\t')
-        errors = ["NOT_INCLUDED", "INCONSISTENT_PARAMETERS", "EVENTS_TSV_MISSING", "TASK_NAME_CONTAIN_ILLEGAL_CHARACTER"] ## Enter errors that need to be removed here 
+        errors = ["NOT_INCLUDED", "INCONSISTENT_PARAMETERS", "EVENTS_TSV_MISSING", "TASK_NAME_CONTAIN_ILLEGAL_CHARACTER", "SLICE_TIMING_NOT_DEFINED", "TASK_NAME_MUST_DEFINE"] ## Enter errors that need to be removed here 
         data = [row for row in reader if not any(val in row for val in errors)]
 
     with open(tsv_output, 'w', newline='') as output_file:
@@ -18,6 +19,7 @@ def remove_lines(tsv_input, tsv_output):
         writer.writerows(data)
 
 def filter_csv(input_csv, output_csv):
+    """Pulls out any lines containing the desired error string"""
     with open(input_csv, 'r', newline='') as input_file, open(output_csv, 'w', newline='') as output_file:
         reader = csv.reader(input_file)
         writer = csv.writer(output_file)
@@ -26,9 +28,34 @@ def filter_csv(input_csv, output_csv):
             if 'INTENDED_FOR' in ','.join(row): ## Enter error that you want to grab here
                 writer.writerow(row)
 
+def remove_duplicates(input_file):
+    # Read lines from the input file
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+
+    # Remove duplicates while preserving order
+    unique_lines = list(dict.fromkeys(lines))
+
+    # Write back to the same file
+    with open(input_file, 'w') as file:
+        file.writelines(unique_lines)
+
 if __name__ == "__main__":
-    input_csv = "/home/rando149/shared/projects/rae_testing/GE_issue_testing/GE-cubids/slurm_cubids_with_s3/GE-reprocessed-cubids.csv"
-    output_csv = "/home/rando149/shared/projects/rae_testing/GE_issue_testing/GE-cubids/slurm_cubids_with_s3/new-part2-GE-issues.csv"
+    input_csv = input("Enter the csv you want to filter (then press enter): ")
+    output_csv = input("Enter the path where the output csv should go (then press enter): ")
+
+    print("Choose an option for which function you want. 1 is for removing lines with unwanted error strings. 2 is for grabbing lines with a specified error")
+
+    choice = input("Enter 1 or 2: ")
+
+    if choice == "1":
+        remove_lines(input_csv, output_csv)
+        remove_duplicates(output_csv)
+    elif choice == "2":
+        filter_csv(input_csv, output_csv)
+        remove_duplicates(output_csv)
+    else:
+        print("Invalid Choice")
 
     #remove_lines(input_csv, output_csv)
 											## Select which function you need here
